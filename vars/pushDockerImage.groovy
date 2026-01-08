5bcff04ac6a213f3d22a5cd822c881fa6d77fa5c
HaidarGhanem <haidar23ghanem@gmail.com>
2026-01-08 14:31:17 +0300

    added dockerId param

--- vars/pushDockerImage.groovy
+++ vars/pushDockerImage.groovy
@@ -1,10 +1,11 @@
 #!/user/bin/env groovy
 
 def call(Map config = [:]) {
+    def dockerId = config.dockerId
     def user = config.user
     def image = config.image
     echo "Pushing Docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-id', passwordVariable: 'PASS', usernameVariable: 'USER')])
+    withCredentials([usernamePassword(credentialsId: "${dockerId}", passwordVariable: 'PASS', usernameVariable: 'USER')])
     {
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh "docker push ${user}/${image}:${env.BUILD_NUMBER}"
