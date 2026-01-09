9072530105e7bb93a51f70294e685519dd3b8f7a
HaidarGhanem <haidar23ghanem@gmail.com>
2026-01-09 10:30:03 +0300

    updated to be manual

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -3,8 +3,6 @@
 def call(Map config = [:]) {
     def user = config.user
     def image = config.image
-    def commitHash = sh(script: "git rev-parse --short HEAD", returnStdout: true).trim()
-    def tag = config.tag ?: commitHash
     echo "Building Docker image..."
-    sh "docker build -t ${user}/${image}:${tag} ."
+    sh "docker build -t ${user}/${image} ."
 }
